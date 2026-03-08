8fe2261efa489c342e293e9aa7c3f9c1f75590de
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-03-08 10:10:40 +0300

    Restructured the code to export to svg.

--- src/main/java/Amr/Annotation/TestAmrFrame.java
+++ src/main/java/Amr/Annotation/TestAmrFrame.java
@@ -2,6 +2,7 @@ package Amr.Annotation;
 
 import Amr.Corpus.AmrCorpus;
 import Amr.Corpus.AmrSentence;
+import AnnotatedSentence.*;
 
 import java.awt.*;
 import java.io.File;
@@ -10,50 +11,50 @@ import java.io.PrintWriter;
 
 public class TestAmrFrame {
 
-    private void gbDataset() throws FileNotFoundException {
+    private static void outputDataSet(AmrCorpus corpus, AnnotatedCorpus corpus2, PrintWriter output, String path){
+        int j = 0;
+        for (int i = 0; i < corpus.sentenceCount(); i++) {
+            AmrDiagram diagram = new AmrDiagram(path, ((AmrSentence) corpus.getSentence(i)).getRawFileName());
+            while (j < corpus2.sentenceCount() && !((AnnotatedSentence)corpus2.getSentence(j)).getFileName().equals(((AmrSentence) corpus.getSentence(i)).getRawFileName())){
+                j++;
+            }
+            Rectangle box = diagram.getBoundingBox();
+            output.print("<h3>");
+            output.println(((AmrSentence) corpus.getSentence(i)).getRawFileName());
+            output.println("</h3>");
+            output.print("<h4>");
+            output.println(((AnnotatedSentence) corpus2.getSentence(j)).toWords());
+            output.println("</h4>");
+            output.println(diagram.toSvg(box.y + box.height, box.x + box.width));
+        }
+    }
+
+    private static void gbDataset() throws FileNotFoundException {
         PrintWriter output = new PrintWriter(new File("output.html"));
         AmrCorpus corpus = new AmrCorpus(new File("../../Gb/Amr/"));
-        for (int i = 0; i < corpus.sentenceCount(); i++) {
-            AmrDiagram diagram = new AmrDiagram("../../Gb/Amr/", ((AmrSentence) corpus.getSentence(i)).getRawFileName());
-            Rectangle box = diagram.getBoundingBox();
-            output.print("<p>");
-            output.print(((AmrSentence) corpus.getSentence(i)).getRawFileName());
-            output.println("</p>");
-            output.println(diagram.toSvg(box.y + box.height, box.x + box.width));
-        }
+        AnnotatedCorpus corpus2 = new AnnotatedCorpus(new File("../../Gb/Turkish-Phrase/"));
+        outputDataSet(corpus, corpus2, output, "../../Gb/Amr/");
         output.close();
     }
 
-    private void tourismDataset() throws FileNotFoundException {
+    private static void tourismDataset() throws FileNotFoundException {
         PrintWriter output = new PrintWriter(new File("output.html"));
         AmrCorpus corpus = new AmrCorpus(new File("../../Etstur/Amr/"));
-        for (int i = 0; i < corpus.sentenceCount(); i++) {
-            AmrDiagram diagram = new AmrDiagram("../../Etstur/Amr/", ((AmrSentence) corpus.getSentence(i)).getRawFileName());
-            Rectangle box = diagram.getBoundingBox();
-            output.print("<p>");
-            output.print(((AmrSentence) corpus.getSentence(i)).getRawFileName());
-            output.println("</p>");
-            output.println(diagram.toSvg(box.y + box.height, box.x + box.width));
-        }
+        AnnotatedCorpus corpus2 = new AnnotatedCorpus(new File("../../Etstur/Turkish-Phrase/"));
+        outputDataSet(corpus, corpus2, output, "../../Etstur/Amr/");
         output.close();
     }
 
-    private void framenetDataset() throws FileNotFoundException {
+    private static void framenetDataset() throws FileNotFoundException {
         PrintWriter output = new PrintWriter(new File("output.html"));
         File[] listOfFiles = new File("../../FrameNet-Examples/Amr/").listFiles();
         for (File file : listOfFiles) {
             if (file.isDirectory()) {
                 String fileName = file.getName();
                 AmrCorpus corpus = new AmrCorpus(new File("../../FrameNet-Examples/Amr/" + fileName));
+                AnnotatedCorpus corpus2 = new AnnotatedCorpus(new File("../../FrameNet-Examples/Turkish-Phrase/" + fileName));
                 output.println("<h2>" + fileName + "</h2>");
-                for (int i = 0; i < corpus.sentenceCount(); i++) {
-                    AmrDiagram diagram = new AmrDiagram("../../FrameNet-Examples/Amr/" + fileName, ((AmrSentence) corpus.getSentence(i)).getRawFileName());
-                    Rectangle box = diagram.getBoundingBox();
-                    output.print("<p>");
-                    output.print(((AmrSentence) corpus.getSentence(i)).getRawFileName());
-                    output.println("</p>");
-                    output.println(diagram.toSvg(box.y + box.height, box.x + box.width));
-                }
+                outputDataSet(corpus, corpus2, output, "../../FrameNet-Examples/Amr/" + fileName);
             }
         }
         output.close();
